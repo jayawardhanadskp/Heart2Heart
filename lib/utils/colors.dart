8bb006de3e02743803673f7801949fe26888c7b6
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-12-06 15:51:26 +0530

    utils

---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color pink = Color.fromARGB(255, 242, 184, 215);
+  static const Color pink1 = Color(0xFFF7F5FC);
+  static const Color blue = Color.fromARGB(255, 192, 176, 234);
+
+  static const Color black = Colors.black;
+  static const Color white = Colors.white;
+}
