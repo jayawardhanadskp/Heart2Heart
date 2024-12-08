92fd7371c0cefa27fb9e4e57ddea47e2c65a617e
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-12-08 14:53:00 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:paint/firebase_options.dart';
 import 'package:paint/pages/login_page.dart';
+import 'package:paint/providers/auth_provider.dart';
 import 'package:paint/providers/drawing_provider.dart';
 import 'package:provider/provider.dart';
 import 'pages/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => DrawingProvider(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => GFAuthProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => DrawingProvider(),
+        ),
+      ],
       child: MyApp(),
     ),
   );
@@ -17,10 +31,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Drawing App',
-      debugShowCheckedModeBanner: false,
-      // theme: ThemeData(primarySwatch: Colors.blue),
-      home: LoginPage(),
+      title: 'Flutter App',
+      initialRoute: '/', // Starting route
+      onGenerateRoute: (RouteSettings settings) {
+        // Check the route name and return appropriate route
+        switch (settings.name) {
+          case '/':
+            return MaterialPageRoute(builder: (context) => LoginPage());
+          case '/home':
+            return MaterialPageRoute(builder: (context) => MyHomePage());
+          case '/profile':
+            // Passing arguments to ProfilePage
+            final String userId = settings.arguments as String;
+            // return MaterialPageRoute(
+              // builder: (context) => ProfilePage(userId: userId),
+            // );
+          default:
+            return MaterialPageRoute(builder: (context) => LoginPage());
+        }
+      },
     );
   }
 }
